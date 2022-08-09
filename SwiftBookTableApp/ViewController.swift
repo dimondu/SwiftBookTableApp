55ac37dd6985f6300c263f75ac6629121c620124
Durov Dmitrii <durov-dmitrii@mail.ru>
2022-08-09 15:45:37 +0300

    Initial Commit

---
+++ SwiftBookTableApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftBookTableApp
+//
+//  Created by Дмитрий Дуров on 09.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
